08da5effaf4fe11e183c15b8221967878a6a719f
Hugo ROUTY <hugo.routy@viacesi.fr>
2020-06-08 09:39:27 +0200

    Ajoutez des fichiers projet.

---
+++ CardGame/Carte.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Carte
+    {
+        private string value;
+        private string suit;
+        private int score;
+
+
+        public Carte (string value, string suit) 
+        {
+            this.value = value;
+            this.suit = suit;
+
+            bool bConvertValue = int.TryParse(this.value, out this.score);
+
+            if (!bConvertValue)
+            {
+                switch (value.ToLower())
+                {
+                    case "as":
+                        this.score = 1;
+                        break;
+                    case "valet":
+                        this.score = 11;
+                        break;
+                    case "reine":
+                        this.score = 12;
+                        break;
+                    case "roi":
+                        this.score = 13;
+                        break;
+                    default:
+                        throw new ArgumentException(paramName: nameof(value), message: "Valeur de carte incorrecte");
+                }
+            }
+        }
+
+
+        public string Value
+        {
+            get { return value; }
+        }
+        public int Score
+        {
+            get { return score; }
+        }
+
+        public string Suit
+        {
+            get { return suit; }
+        }
+
+    }
+}
---
+++ CardGame/JeuDeCartes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class JeuDeCartes
+    {
+        private List<Carte> jeuDeCartes;
+        private List<string> couleurs = new List<string> { "Pique", "Trefle", "Coeur", "Carreau"};
+        private List<string> figures = new List<string> { "Valet", "Reine", "Roi"};
+
+
+        public JeuDeCartes()
+        {
+            foreach (string couleur in couleurs)
+            {
+                for (int i = 1; i<11; i++)
+                {
+
+                }
+
+                foreach (string figure in figures)
+                {
+
+                }
+            }
+           
+        }
+
+        public List<Carte> ListeCartes
+        {
+            get { return jeuDeCartes; }
+        }
+
+    }
+}
